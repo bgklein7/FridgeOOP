747ab0bfa5335c3d2bc98bbcf714e457b760fb18
bgklein7 <bgklein@outlook.com>
2018-02-01 09:57:53 -0500

    created user menu

--- FridgeOOP/Program.cs
+++ FridgeOOP/Program.cs
@@ -10,6 +10,30 @@ namespace FridgeOOP
     {
         static void Main(string[] args)
         {
+            //create an interactive console window
+            //Prompt user to choose what to do with the fridge
+            //Call methods on Fridge object to do what the user wants
+
+            int userChoice;
+            do
+            {
+
+                Console.WriteLine("Hello, welcome to the fridge app.");
+                Console.WriteLine("What would you like to do with your fridge?");
+                Console.WriteLine("To dispense water, type 1.");
+                Console.WriteLine("To change the lightbulb, type 2.");
+                Console.WriteLine("To clean, type 3.");
+                Console.WriteLine("To take food, type 4.");
+                Console.WriteLine("To check food supplies, type 5.");
+                Console.WriteLine("To see all fridge details, type 6.");
+                Console.WriteLine("To restart, type 7.");
+                Console.WriteLine("To quit, type 8.");
+
+                userChoice = int.Parse(Console.ReadLine());
+
+            } while (userChoice != 8);
+
         }
     }
+    
 }
